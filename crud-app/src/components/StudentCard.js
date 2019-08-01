f031bf0c86004c833448884f8f856f8fd6132f26
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2019-08-01 14:39:02 -0400

    clockable lonk for each student in student grid closes #15

--- crud-app/src/components/StudentCard.js
+++ crud-app/src/components/StudentCard.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import './StudentCard.css';
+import { Link } from 'react-router-dom';
 
 class StudentCard extends Component{
 	render(){
+		let thisStudentIdLink = "/students/" + this.props.studentId;
 		return(
 			<div className="studentInfo">
+				<Link to={thisStudentIdLink}>
 				<div className="studentImage">
 					<img src={this.props.imageLink} alt="Student Pic" />
 				</div>
@@ -14,6 +17,7 @@ class StudentCard extends Component{
 				<div className="campusName">
 					{this.props.getCampusName(this.props.campusId)}
 				</div>
+				</Link>
 			</div>
 		);
 	}
