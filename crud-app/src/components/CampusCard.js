9ce4c6be7617b7230d6b3499fbf2804492e47bcc
mehrabha <mehrabhfz@gmail.com>
2019-08-04 15:03:20 -0700

    changed redirect to link

--- crud-app/src/components/CampusCard.js
+++ crud-app/src/components/CampusCard.js
@@ -8,41 +8,25 @@ import {Link} from 'react-router-dom'
 class CampusCard extends React.Component {
 	constructor(props) {
     super(props);
-    this.state = {
-      view: false,
-      redirectUrl: ""
-    };
     this.removeCampus = this.removeCampus.bind(this);
-		this.viewCampus = this.viewCampus.bind(this);
 	}
 
 	removeCampus() {
 		this.props.removeCampus(this.props.campus);
   }
-  
-  viewCampus() {
-    let newUrl = "./campuses/" + this.props.campus.id;
-    this.setState({view: true, redirectUrl: newUrl});
-  }
+
     render() {
-      if (!this.state.view) {
         return(
             <div className="CampusCard">
               <img className="campus-card-img" src={this.props.campus.img}/>
               <div className="campus-card-info">
-                <div className="campus-card-title">{this.props.campus.name}</div>
+                <Link className="campus-card-title" to={"/campuses/" + this.props.campus.id}>{this.props.campus.name}</Link>
                 <div className="campus-card-element">{this.props.campus.bio}</div>
                 <button className="campus-card-element"><Link to={"/campuses/" + this.props.campus.id + "/edit"}>edit</Link></button>
                 <button className="campus-card-element" onClick={this.removeCampus}>remove</button>
-                <button className="campus-card-element" onClick={this.viewCampus}>view</button>
               </div>
             </div>
         );
-      } else {
-        return(
-            <Redirect to={this.state.redirectUrl}/>
-        );
-      }
     }
 }
 
