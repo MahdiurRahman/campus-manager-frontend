9e2728945df15bd79dbe5f0fc51bf2908afde9ff
Mahdiur Rahman <rmahdiur@gmail.com>
2019-08-04 14:09:59 -0400

    close #21

--- crud-app/src/reducers/index.js
+++ crud-app/src/reducers/index.js
@@ -34,7 +34,94 @@ const selectedCampusReducer = (selectedCampus = null, action) => {
 	return selectedCampus;
 };
 
+let students = 
+[
+	{
+	  id: 0,
+	  name: "Neil Tyson",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 2.0,
+	  college: 0
+	},
+	{
+	  id: 1,
+	  name: "Mike Tyson",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 2.2,
+	  college: 0
+	},
+	{
+	  id: 2,
+	  name: "Elon Musk",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 2.7,
+	  college: 0
+	},
+	{
+	  id: 3,
+	  name: "Elizabeth Ryler",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 2.9,
+	  college: 1
+	},
+	{
+	  id: 4,
+	  name: "Harold Kimp",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 3.3,
+	  college: 1
+	},
+	{
+	  id: 5,
+	  name: "Michelle Rubin",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 1.0,
+	  college: 1
+	},
+	{
+	  id: 6,
+	  name: "Kenneth Di",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 0.1,
+	  college: 1
+	},
+	{
+	  id: 7,
+	  name: "Lolita Lopez",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 3.7,
+	  college: 1
+	},
+	{
+	  id: 8,
+	  name: "Rain Man",
+	  img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+	  gpa: 4.0,
+	  college: 0
+	}
+]
+
+const studentsReducer = (oldStudentsList = students, action) => {
+	switch (action.type) {
+		case 'ADD_STUDENT':
+			return [...oldStudentsList, action.payload];
+		case 'DELETE_STUDENT':
+			return oldStudentsList.filter(student => (student.id != action.payload.id))	
+		default:
+			return oldStudentsList
+	}
+}
+
+const selectedStudentReducer = (selectedStudent = null, action) => {
+	if (action.type == 'SELECT_STUDENT') {
+		return action.payload
+	}
+	return selectedStudent
+}
+
 export default combineReducers({
 	campuses: campusesReducer,
-	selectedCampus: selectedCampusReducer
+	selectedCampus: selectedCampusReducer,
+	students: studentsReducer,
+	selectedStudent: selectedStudentReducer
 });
