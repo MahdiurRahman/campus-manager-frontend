e78a494665eedee685deb3398816cfee13477ba7
mehrabha <mehrabhfz@gmail.com>
2019-08-04 10:57:28 -0700

    campus view link

--- crud-app/src/components/CampusCard.js
+++ crud-app/src/components/CampusCard.js
@@ -1,18 +1,30 @@
 import React from "react"
 import "./CampusCard.css"
 import {connect} from "react-redux"
+import {Redirect} from 'react-router';
 import {removeCampus} from '../actions/index.js'
 
 class CampusCard extends React.Component {
 	constructor(props) {
-		super(props);
-		this.removeCampus = this.removeCampus.bind(this);
+    super(props);
+    this.state = {
+      view: false,
+      redirectUrl: ""
+    };
+    this.removeCampus = this.removeCampus.bind(this);
+		this.viewCampus = this.viewCampus.bind(this);
 	}
 
 	removeCampus() {
 		this.props.removeCampus(this.props.campus);
-	}
+  }
+  
+  viewCampus() {
+    let newUrl = "./campuses/" + this.props.campus.id;
+    this.setState({view: true, redirectUrl: newUrl});
+  }
     render() {
+      if (!this.state.view) {
         return(
             <div className="CampusCard">
               <img className="campus-card-img" src={this.props.campus.img}/>
@@ -21,9 +33,15 @@ class CampusCard extends React.Component {
                 <div className="campus-card-element">{this.props.campus.bio}</div>
                 <button className="campus-card-element">edit</button>
                 <button className="campus-card-element" onClick={this.removeCampus}>remove</button>
+                <button className="campus-card-element" onClick={this.viewCampus}>view</button>
               </div>
             </div>
         );
+      } else {
+        return(
+            <Redirect to={this.state.redirectUrl}/>
+        );
+      }
     }
 }
 
--- crud-app/src/reducers/index.js
+++ crud-app/src/reducers/index.js
@@ -2,13 +2,13 @@ import {combineReducers} from 'redux';
 
 let campuses = [
 	{
-		id: 0,
+		id: "0",
 		name: "hunter",
 		bio: "Cuny",
 		address: "123 Main St",
 		img: "https://png.pngtree.com/svg/20170616/22811e059c.svg"},
 	{
-		id: 1,
+		id: "1",
 		name: "baruch",
 		bio: "Cuny",
 		address: "456 Main St",
