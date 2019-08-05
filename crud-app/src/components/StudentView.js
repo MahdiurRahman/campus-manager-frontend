e69bab34e9765ab770d28c9c11b3c76efc65389b
Mahdiur Rahman <rmahdiur@gmail.com>
2019-08-05 11:14:44 -0400

    fixed view bugs

--- crud-app/src/components/StudentView.js
+++ crud-app/src/components/StudentView.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import EditStudent from './EditStudent';
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
-function StudentView(props) {
-    return (
+class StudentView extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        let student;
+        console.log(this.props.student_id)
+        for (let i = 0; i < this.props.students.length; i++) {
+            if (this.props.students[i].id == this.props.student_id) {
+                student = this.props.students[i]
+            }
+        }
+        return (
             <div>
-            <p>{props.name}</p>
-            <img src={props.img} />
-            <p>{props.gpa}</p>
+            <p>{student.name}</p>
+            <img src={student.img} />
+            <p>{student.gpa}</p>
             <p>This student is registered to:</p>
             {/* INSERT CAMPUS CARD OVER HERE WITH A PROP SENDING TO CORRECT CAMPUS */}
-            <button><Link to={"/students/" + props.id + "/edit"}>Edit</Link></button>
+            <button><Link to={"/students/" + student.id + "/edit"}>Edit</Link></button>
             <button>LINK AND FUNCTION TO DELETE</button>
             </div>
             )
+    }
 }
 
-export default StudentView
+const mapStateToProps = state => {
+    return {
+        students: state.students
+    }
+}
+
+export default connect(mapStateToProps, {})(StudentView)
