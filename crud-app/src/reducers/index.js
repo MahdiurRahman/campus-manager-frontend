a97a1d5d56bb5f9f397bc905b7b1249e79a81bae
mehrabha <mehrabhfz@gmail.com>
2019-08-05 00:14:21 -0400

    add student to campus feature

--- crud-app/src/reducers/index.js
+++ crud-app/src/reducers/index.js
@@ -22,63 +22,63 @@ let students = [
         name: "Neil Tyson",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 2.0,
-        college: 0
+        college: "0"
     },
     {
         id: 1,
         name: "Mike Tyson",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 2.2,
-        college: 0
+        college: "0"
      },
      {
         id: 2,
         name: "Elon Musk",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 2.7,
-        college: 0
+        college: "0"
      },
      {
         id: 3,
         name: "Elizabeth Ryler",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 2.9,
-        college: 1
+        college: "1"
     },
     {
         id: 4,
         name: "Harold Kimp",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 3.3,
-        college: 1
+        college: "1"
     },
     {
          id: 5,
         name: "Michelle Rubin",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 1.0,
-        college: 1
+        college: "1"
     },
     {
         id: 6,
         name: "Kenneth Di",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 0.1,
-        college: 1
+        college: "1"
     },
     {
         id: 7,
         name: "Lolita Lopez",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 3.7,
-        college: 1
+        college: "1"
     },
     {
         id: 8,
         name: "Rain Man",
         img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
         gpa: 4.0,
-        college: 0
+        college: "0"
     }
 ];
 
@@ -111,6 +111,8 @@ const studentsReducer = (oldListOfStudents = students, action) => {
             return oldListOfStudents;
     }
 };
+
+
 
 export default combineReducers({
 	campuses: campusesReducer,
