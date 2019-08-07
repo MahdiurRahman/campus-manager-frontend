282c629601a4e949e49517b1ec7605e560d67566
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2019-08-07 16:26:06 -0400

    added input checkers for editStudent and addStudent

--- src/components/StudentTab/StudentAddForm.js
+++ src/components/StudentTab/StudentAddForm.js
@@ -7,6 +7,7 @@ class StudentAddForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            nameIsCorrect: true,
             redirect: false
         }
         this.addStudent = this.addStudent.bind(this);
@@ -14,18 +15,32 @@ class StudentAddForm extends Component {
     
     addStudent (e) {
         e.preventDefault();
-        
-        let student = {
-            name: e.target.elements.name.value,
-            img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg'
+        let correctName = true;
+        if (e.target.elements.name.value.length < 1){
+            correctName = false;
         }
-        this.props.addStudent(student);
-        this.setState({redirect: true});
+        for (let i = 0; i < e.target.elements.name.value.length; i++){
+            if (!(e.target.elements.name.value[i].toLowerCase() != e.target.elements.name.value[i].toUpperCase() || e.target.elements.name.value[i] == " ")){
+                correctName = false;
+            }
+        }
+        this.setState({
+            nameIsCorrect: correctName
+        })
+        if (correctName){
+            let student = {
+                name: e.target.elements.name.value,
+                img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg'
+            }
+            this.props.addStudent(student);
+            this.setState({redirect: true});
+        }
     }
     
     render() {
         if (!this.state.redirect) {
             return (
+                <div className="result">
                     <form onSubmit={this.addStudent}>
                         <label>
                             Name:
@@ -33,6 +48,12 @@ class StudentAddForm extends Component {
                         </label>
                         <input type="submit" value="Submit"/>
                     </form>
+                    <div className="inputErrors">
+                    {this.state.nameIsCorrect ?
+                    "" :
+                    "Name must be at least one character long and must contain only letters and spaces"}
+                    </div>
+                    </div>
                     );
         } else {
             return (
